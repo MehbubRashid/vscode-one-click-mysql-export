5a972af3044b48859d0f955b66a463501dcbbeaa
mehbubrashid <mehbub.rabu@gmail.com>
2022-05-13 03:44:18 +0600

    working copy

--- extension.js
+++ extension.js
@@ -1,6 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+var _ = require("lodash");
+var fs = require("fs");
+var path = require('path');
+var mysqldump = require('mysqldump');
+const {execSync} = require('child_process');
+
 
 let myStatusBarItem;
 
@@ -14,18 +20,81 @@ function activate({ subscriptions }) {
 
 	// register a command that is invoked when the status bar
 	// item is selected
-	const myCommandId = 'sample.showSelectionCount';
+	const myCommandId = 'vscode-one-click-mysql-export.export';
 	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
-		const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
-		vscode.window.showInformationMessage(`Yeah, ${n} line(s) selected... Keep going!`);
+		var msg = '';
+		if(vscode.workspace.workspaceFolders !== undefined) {
+			let openedFolderPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+			let configDir = openedFolderPath + '/.vscode';
+			let configFile = configDir + '/mysql-export.json';
+
+			if ( !openedFolderPath ) {
+				msg = 'You must open a folder';
+				showErr(msg);
+			}
+			else {
+				var defaults = {
+					"mysqlDumpDir": "C:/wamp64/bin/mysql/mysql8.0.27/bin",
+					"host": "localhost",
+					"user": "root",
+					"pass": "",
+					"port": 3306,
+					"db": "",
+					"destination": "db.sql"
+				};
+
+				var configjson = fs.readFileSync(configFile).toString();
+				var configObject;
+
+				try {
+					configObject = JSON.parse(configjson);
+					var realconfig = _.defaults(configObject, defaults);
+					var destination = path.join(openedFolderPath, realconfig.destination);
+					// var dumper = path.join(realconfig.mysqlDumpDir, 'mysqldump')
+					// var command = `${dumper} -h'${realconfig.host}' -u'${realconfig.user}' -p'${realconfig.pass}' ${realconfig.db} > ${destination}`;
+					// let output = execSync(command);
+					// console.log(output);
+					mysqldump({
+						connection: {
+							host: realconfig.host,
+							user: realconfig.user,
+							password: realconfig.pass,
+							database: realconfig.db,
+						},
+						dumpToFile: destination,
+						dump: {
+							data: {
+								format: false
+							}
+						}
+					});
+				} catch (err) {
+					showErr(err);
+				}
+			}
+		} 
+		else {
+			msg = "Working folder not found, open a folder an try again" ;
+		
+			showMsg(msg);
+		}
 	}));
 
 	// create a new status bar item that we can now manage
 	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	myStatusBarItem.text = `hahah`;
+	myStatusBarItem.text = `Export MySQL`;
 	myStatusBarItem.command = myCommandId;
 	subscriptions.push(myStatusBarItem);
 	myStatusBarItem.show();
+}
+
+
+function showErr(msg) {
+	vscode.window.showErrorMessage(msg);
+}
+
+function showMsg(msg) {
+	vscode.window.showInformationMessage(msg);
 }
 
 // this method is called when your extension is deactivated
